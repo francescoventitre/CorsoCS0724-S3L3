a07f00ba5a96933b9c12d6026452a1bd1d5cf0aa
francescoventitre <francesco.orru23@gmail.com>
2024-11-20 16:04:46 +0100

    Esercizi S3L3

---
+++ encdec.py
@@ -0,0 +1,32 @@
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import serialization
+import base64
+
+# Carica la chiave privata
+with open('private_key.pem', 'rb') as key_file:
+    private_key = serialization.load_pem_private_key(
+        key_file.read(),
+        password=None
+    )
+
+# Carica la chiave pubblica
+with open('public_key.pem', 'rb') as key_file:
+    public_key = serialization.load_pem_public_key(key_file.read())
+
+message = 'Hello, I am ILLVENTITRE!'
+
+# Criptazione con la chiave pubblica
+encrypted = public_key.encrypt(
+    message.encode(),
+    padding.PKCS1v15()
+)
+
+# Decriptazione con la chiave privata
+decrypt = private_key.decrypt(
+    encrypted,
+    padding.PKCS1v15()
+)
+
+print("Messaggio originale:", message)
+print("Messaggio criptato:", base64.b64encode(encrypted).decode('utf-8'))
+print("Messaggio decriptato:", decrypt.decode('utf-8'))
